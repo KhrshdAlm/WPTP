79bcadb291b5041a7fc15f35bd8270619cb7462d
Khurshid Alam <khurshid@pc-5304.kl.dfki.de>
2018-07-31 17:27:47 +0200

    add ns3 project

---
+++ ns-3-dev/scratch/ptp-pcf.cpp
@@ -0,0 +1,19 @@
+#include "ns3/log.h"
+#include "ns3/config.h"
+#include "ns3/command-line.h"
+#include "ns3/uinteger.h"
+#include "ns3/boolean.h"
+#include "ns3/string.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/mobility-helper.h"
+#include "ns3/internet-stack-helper.h"
+#include "ns3/ipv4-address-helper.h"
+#include "ns3/packet-sink-helper.h"
+#include "ns3/on-off-helper.h"
+#include "ns3/packet-sink.h"
+#include "ns3/ssid.h"
+#include "ns3/wifi-mac-header.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("WifiPtp");
